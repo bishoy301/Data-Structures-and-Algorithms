53bc963c498e98a8d0ad82efbd54eb6577f3838f
Bishoy Boktor <bishoyrock2@gmail.com>
2016-01-03 11:39:08 -0600

    Adding the chapter 2 questions

---
+++ Chap02/Intellijtest/src/com/github/bishoy301/things.java
@@ -0,0 +1,7 @@
+package com.github.bishoy301;
+
+/**
+ * Created by bishoyboktor on 1/3/16.
+ */
+public class things {
+}
---
+++ Chap02/Intellijtest/src/test.java
@@ -0,0 +1,5 @@
+/**
+ * Created by bishoyboktor on 1/3/16.
+ */
+public class test {
+}
